dd6c55bd0941487067800ede24e9446cb0fdd77e
RajSingh8293 <singhraj8293@gmail.com>
2024-08-28 04:45:55 -0700

    user controllers and user routes

--- backend/index.js
+++ backend/index.js
@@ -2,47 +2,50 @@ import express from "express";
 const app = express();
 import "dotenv/config";
 import connectDb from "./db/conn.js";
+import userRouter from "./routes/user.routes.js";
 
 const port = process.env.PORT || 3060;
 
 connectDb();
 
-app.get("/", (req, res) => {
-  res.json([
-    {
-      color: "red",
-      value: "#f00",
-    },
-    {
-      color: "green",
-      value: "#0f0",
-    },
-    {
-      color: "blue",
-      value: "#00f",
-    },
-    {
-      color: "cyan",
-      value: "#0ff",
-    },
-    {
-      color: "magenta",
-      value: "#f0f",
-    },
-    {
-      color: "yellow",
-      value: "#ff0",
-    },
-    {
-      color: "black",
-      value: "#000",
-    },
-  ]);
-});
+// app.get("/", (req, res) => {
+//   res.json([
+//     {
+//       color: "red",
+//       value: "#f00",
+//     },
+//     {
+//       color: "green",
+//       value: "#0f0",
+//     },
+//     {
+//       color: "blue",
+//       value: "#00f",
+//     },
+//     {
+//       color: "cyan",
+//       value: "#0ff",
+//     },
+//     {
+//       color: "magenta",
+//       value: "#f0f",
+//     },
+//     {
+//       color: "yellow",
+//       value: "#ff0",
+//     },
+//     {
+//       color: "black",
+//       value: "#000",
+//     },
+//   ]);
+// });
 
-app.get("/about", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
+// app.get("/about", (req, res) => {
+//   res.json({ message: "Hello World!" });
+// });
+
+app.use("/api/v1", userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
