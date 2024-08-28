071ae9ccd8072a47a5171911fddd044776447c1a
RajSingh8293 <singhraj8293@gmail.com>
2024-08-28 03:35:10 -0700

    job poratl web

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+const app = express();
+import "dotenv/config";
+import connectDb from "./db/conn.js";
+
+const port = process.env.PORT || 3040;
+
+connectDb();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.get("/about", (req, res) => {
+  res.json([
+    {
+      color: "red",
+      value: "#f00",
+    },
+    {
+      color: "green",
+      value: "#0f0",
+    },
+    {
+      color: "blue",
+      value: "#00f",
+    },
+    {
+      color: "cyan",
+      value: "#0ff",
+    },
+    {
+      color: "magenta",
+      value: "#f0f",
+    },
+    {
+      color: "yellow",
+      value: "#ff0",
+    },
+    {
+      color: "black",
+      value: "#000",
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
