59c6710b34650a1dd7b17af13ee0afcea8f0a491
RajSingh8293 <92074370+RajSingh8293@users.noreply.github.com>
2024-10-01 08:31:04 +0530

    Update sendResponse.js
    change cookie option

--- backend/utils/sendResponse.js
+++ backend/utils/sendResponse.js
@@ -4,16 +4,20 @@ import { generateToken } from "./generateToken.js";
 export const sendResponse = (res, user, statusCode, message) => {
   const token = generateToken(user._id);
 
-  const options = {
-    httpOnly: true,
-    secure: true,
-    // sameSite: 'None',
-    sameSite: 'Strict'
-  };
+  // const options = {
+  //   httpOnly: true,
+  //   secure: true,
+  //   sameSite: 'None',
+  // };
   const { password: pass, ...rest } = user._doc; // hide password
   return res
     .status(statusCode)
-    .cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000 }, options)
+    .cookie("token", token, { 
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: true,
+      sameSite: 'None',
+    })
     .json({
       success: true,
       message,
