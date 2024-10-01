d8d6a80303bb685c78b39683a01f1de8acb0c925
RajSingh8293 <92074370+RajSingh8293@users.noreply.github.com>
2024-10-01 07:09:36 +0530

    Update Api.js

--- frontend/src/constant/Api.js
+++ frontend/src/constant/Api.js
@@ -1,3 +1,3 @@
-export const backendApi = "https://hospital-web-backend.vercel.app"; // vercel
-// export const backendApi = "https://hospital-web-backend.onrender.com"; // render
+// export const backendApi = "https://hospital-web-backend.vercel.app"; // vercel
+export const backendApi = "https://hospital-web-backend.onrender.com"; // render
 // export const backendApi = "http://localhost:3070"; // local
